faf2073c7b42fe6cee92e99d2bb25d99c67cd463
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-05-09 14:57:30 +0200

    Added api app to INSTALLED_APPS in settings.py

--- django/backend/backend/settings.py
+++ django/backend/backend/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'api.apps.ApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
