886aba5b473fb25528c320f85b581054a49c9ef9
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-05-11 15:01:57 +0200

    added SESSION_COOKIE_SECURE and CSRF_COOKIE_SECURE to django settings

--- django/backend/backend/settings.py
+++ django/backend/backend/settings.py
@@ -28,7 +28,8 @@ else:
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
-
+SESSION_COOKIE_SECURE=True
+CSRF_COOKIE_SECURE=True
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
