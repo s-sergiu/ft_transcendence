b350dd4d0699cc21fd5f00c14a8fb74af77dad5c
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-06-06 00:14:20 +0200

    implemented simple change match function

--- django/backend/api/urls.py
+++ django/backend/api/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("get-token", views.getToken, name="get-token"),
     path("get-info", views.getUserInfo, name="get-info"),
     path("change-info", views.changeInfo, name="change-info"),
+    path("change-match-data", views.changeMatchData, name="change-match-data"),
     path("register", views.register, name="register"),
     path("login", views.login, name="login"),
 ]
