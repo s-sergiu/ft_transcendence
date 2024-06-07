204645e5f5d9f4f2930c0c773614e913dfbc96f0
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-06-07 20:16:26 +0200

    added getMatchWins component

---
+++ react/frontend/src/components/GetMatchWins.js
@@ -0,0 +1,45 @@
+
+import { useEffect, useState } from 'react';
+
+var URL = process.env.REACT_APP_HTTP_METHOD + "://" + process.env.REACT_APP_HOST_NAME + ":" + process.env.REACT_APP_DJANGO_PORT
+if (process.env.REACT_APP_HTTP_METHOD === 'https')
+	URL = process.env.REACT_APP_HTTP_METHOD + "://" + process.env.REACT_APP_HOST_NAME
+
+const GetMatchWins = (body) =>  {
+
+	let csrf;
+	const [ wins, setWins ] = useState();
+
+	try {
+		csrf = document.cookie.match(("(^|;)\\s*csrftoken\\s*=\\s*([^;]+)"))[2];
+	} catch (err) {
+		console.error(err.message);
+	}
+	const fetchWins = async () => {
+		try {
+			const response = await fetch(URL + '/api/get-match-wins', {
+			  mode:  'cors',
+			  method: 'POST',
+			  credentials: 'include',
+			  body: JSON.stringify({
+				code: body
+			  }),
+			  headers: {
+				"X-CSRFToken": csrf,
+				'Content-Type': 'application/json'
+			  },
+			})
+			const resp = await response.json();
+			setWins(resp);
+		} catch (error) { 
+			console.error(error) 
+		}
+	}
+	useEffect (() => {
+		fetchWins()
+	}, [])
+	return { wins }
+}
+
+export default GetMatchWins;
+
