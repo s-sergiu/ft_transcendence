84b2de6a56c1962c5f7078571d54f55601c2225d
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-06-07 15:20:31 +0200

    implemented getMatchData component

---
+++ react/frontend/src/components/GetMatchData.js
@@ -0,0 +1,45 @@
+
+import { useEffect, useState } from 'react';
+
+var URL = process.env.REACT_APP_HTTP_METHOD + "://" + process.env.REACT_APP_HOST_NAME + ":" + process.env.REACT_APP_DJANGO_PORT
+if (process.env.REACT_APP_HTTP_METHOD === 'https')
+	URL = process.env.REACT_APP_HTTP_METHOD + "://" + process.env.REACT_APP_HOST_NAME
+
+const GetMatchData = (body) =>  {
+
+	let csrf;
+	const [ data, setData ] = useState();
+
+	try {
+		csrf = document.cookie.match(("(^|;)\\s*csrftoken\\s*=\\s*([^;]+)"))[2];
+	} catch (err) {
+		console.error(err.message);
+	}
+	const fetchData = async () => {
+		try {
+			const response = await fetch(URL + '/api/get-match-data', {
+			  mode:  'cors',
+			  method: 'POST',
+			  credentials: 'include',
+			  body: JSON.stringify({
+				code: body
+			  }),
+			  headers: {
+				"X-CSRFToken": csrf,
+				'Content-Type': 'application/json'
+			  },
+			})
+			const resp = await response.json();
+			setData(resp);
+		} catch (error) { 
+			console.error(error) 
+		}
+	}
+	useEffect (() => {
+		fetchData()
+	}, [])
+	return { data }
+}
+
+export default GetMatchData;
+
