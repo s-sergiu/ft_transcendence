9e9bfb880e599d3f4caad735d57698d7398f6966
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-06-07 20:16:09 +0200

    added getMatchLoss component

---
+++ react/frontend/src/components/GetMatchLoss.js
@@ -0,0 +1,45 @@
+
+import { useEffect, useState } from 'react';
+
+var URL = process.env.REACT_APP_HTTP_METHOD + "://" + process.env.REACT_APP_HOST_NAME + ":" + process.env.REACT_APP_DJANGO_PORT
+if (process.env.REACT_APP_HTTP_METHOD === 'https')
+	URL = process.env.REACT_APP_HTTP_METHOD + "://" + process.env.REACT_APP_HOST_NAME
+
+const GetMatchLoss = (body) =>  {
+
+	let csrf;
+	const [ loss, setLoss ] = useState();
+
+	try {
+		csrf = document.cookie.match(("(^|;)\\s*csrftoken\\s*=\\s*([^;]+)"))[2];
+	} catch (err) {
+		console.error(err.message);
+	}
+	const fetchLoss = async () => {
+		try {
+			const response = await fetch(URL + '/api/get-match-loss', {
+			  mode:  'cors',
+			  method: 'POST',
+			  credentials: 'include',
+			  body: JSON.stringify({
+				code: body
+			  }),
+			  headers: {
+				"X-CSRFToken": csrf,
+				'Content-Type': 'application/json'
+			  },
+			})
+			const resp = await response.json();
+			setLoss(resp);
+		} catch (error) { 
+			console.error(error) 
+		}
+	}
+	useEffect (() => {
+		fetchLoss()
+	}, [])
+	return { loss }
+}
+
+export default GetMatchLoss;
+
