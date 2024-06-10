63e56e5d513b71f3c868b3dc4dd538be96821db1
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-06-10 05:03:57 +0200

    fixed case sensitive path

--- react/frontend/src/components/Status/statusDisplay.js
+++ react/frontend/src/components/Status/statusDisplay.js
@@ -1,4 +1,4 @@
-import { set, update } from 'lodash';
+import { update } from 'lodash';
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
@@ -8,33 +8,35 @@ if (process.env.REACT_APP_HTTP_METHOD === 'http') {
 	URL = process.env.REACT_APP_HTTP_METHOD + "://" + process.env.REACT_APP_HOST_NAME + ":4000";
 	socket = io(URL);
 } else {
-	URL = process.env.REACT_APP_HTTP_METHOD + "://" + process.env.REACT_APP_HOST_NAME 
+	URL = process.env.REACT_APP_HTTP_METHOD + "://" + process.env.REACT_APP_HOST_NAME
 	socket = io(URL, {   path: "/socket.io" });
 }
 
 const UserStatusLED = ({ userName }) => {
   const [status, setStatus] = useState('Offline');
-  const [ledColor, setLedColor] = useState('red');
 
-  const update = () => {
+  useEffect(() => {
+
     socket.emit('checkStatus', userName);
 
     socket.on('statusUpdate', (data) => {
-        if (data.login === userName){
-          setStatus(data.status);
-          setLedColor(data.status === 'Online'? 'green' :'red');
-        }
+        if (data.login === userName)
+            setStatus(data.status);
     });
     return () => {
       socket.close();
     };
-  };
+  }, [status]);
 
+
+
+  const renderLEDColor = () => {
+    return status === 'Online' ? 'green' : 'red';
+  };
 
   return (
     <div>
-      <button onClick={update} style={{ fontSize: '12px', padding: '5px 10px' }}>Update</button>
-      <div style={{  marginTop: '-25px', width: '20px', height: '20px', borderRadius: '50%', backgroundColor: ledColor }}></div>
+      <div style={{ width: '20px', height: '20px', borderRadius: '50%', backgroundColor: renderLEDColor() }}></div>
     </div>
   );
 };
