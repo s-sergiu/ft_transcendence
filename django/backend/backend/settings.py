ecb3bc036ede4dc63b80e1094642dada78bb2087
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-06-11 08:19:11 +0200

    added mediat root and media url for imagefield

--- django/backend/backend/settings.py
+++ django/backend/backend/settings.py
@@ -139,6 +139,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# URL used to access the media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'mediafiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
