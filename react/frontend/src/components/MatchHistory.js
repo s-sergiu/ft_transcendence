7abe8729e7390b8227022e8d4ec72307bfe0aefa
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-06-12 07:43:02 +0200

    revert fix

--- react/frontend/src/components/MatchHistory.js
+++ react/frontend/src/components/MatchHistory.js
@@ -5,7 +5,6 @@ import { useState, useEffect } from 'react';
 function MatchHistory (props) {
 	const { user } = props;
 	const { data } = GetMatchData(user);
-	var date = new Date();
 	if (data) {
 		var test = JSON.parse(data);
 	}
@@ -32,7 +31,7 @@ function MatchHistory (props) {
 						  <td>{res['fields'].player2}</td>
 						  <td>{res['fields'].score1}</td>
 						  <td>{res['fields'].score2}</td>
-						  <td>{date.toUTCString(res['fields'].timenow)}</td>
+						  <td>{res['fields'].timenow}</td>
 						  <td>{res['fields'].winner}</td>
 						</tr>
 					))}
